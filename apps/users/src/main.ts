d06bbb24089a03ff62ea23c5e8727fedd124ba52
kekpetsou <stan.ekpetsou@digital.gouv.tg>
2025-11-24 23:30:21 +0000

    init(apps microservices architecture libray):initialization of project and microservices architecure setup

---
+++ apps/users/src/main.ts
@@ -0,0 +1,18 @@
+import * as dotenv from 'dotenv';
+import { NestFactory } from '@nestjs/core';
+import { UsersModule } from './users.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    UsersModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 3000,
+      },
+    }
+  );
+  await app.listen();
+}
+bootstrap();
